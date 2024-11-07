65b2bd1e5bec1b3d71c0b58929df1b33aec7b204
M.BasilKhan <basilkhan1250@gmail.com>
2024-11-07 21:01:33 +0500

    working on it

--- src/components/DynamicComponent.jsx
+++ src/components/DynamicComponent.jsx
@@ -1,14 +1,23 @@
 import React from "react";
+import img1 from "./assets/content1-img.jpg"
 
 const DynamicComponent = (props) => {
 
     return (
         <div className="dynamic-data">
-            <p className="name">{props.names}</p>
-            <h2 className="title">{props.title}</h2>
-            <p className="content">{props.content}</p>
-
-            <span className="data-line"></span>
+            <div className="text-content">
+                <p className="name">{props.names}</p>
+                <h2 className="title">{props.title}</h2>
+                <p className="content">{props.content}</p>
+                <div className="other-data">
+                    <p>{props.date}</p>
+                    <p>{props.likes}</p>
+                    <p>{props.comments}</p>
+                </div>
+            </div>
+            <div className="img-div">
+                <img src={props.image} alt="imgaes" />
+            </div>
         </div>
     )
 
