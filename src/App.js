d85d51e49d2d20e1354e868c1af32a8781da7c86
M.BasilKhan <basilkhan1250@gmail.com>
2024-11-22 01:54:12 +0500

    working on it

--- src/App.js
+++ src/App.js
@@ -4,18 +4,23 @@ import { Routes, Route } from 'react-router';
 import AnotherDev from './components/AnotherData';
 import Blogs from './components/Blogs';
 import NotFoundData from './components/NotFoundData';
+import DataProvider from './components/DataComponent';
 
 function App() {
+  // <Blogs />
+
   return (
-    <div>
+    // <div>
+    <DataProvider>
       <Routes>
         <Route path='/' element={<Navbar />}></Route>
         <Route path='/blogs' element={<AnotherDev />}>
-          <Route path=":number" element={<Blogs />}></Route> 
+          <Route path=":number" element={<Blogs />}></Route>
         </Route>
         <Route path='*' element={<NotFoundData />}></Route>
       </Routes>
-    </div>
+    </DataProvider>
+    // </div>
   );
 }
 
