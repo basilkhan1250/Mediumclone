6c12a93a65c269106f8a1760ba396a94fd5d66d4
M.BasilKhan <basilkhan1250@gmail.com>
2024-12-21 05:12:26 +0500

    working on it

--- src/components/DataComponent.jsx
+++ src/components/DataComponent.jsx
@@ -10,12 +10,12 @@ const DataProvider = ({ children }) => {
     useEffect(() => {
         async function getArticles() {
             try {
-                const response = await fetch("https://newsapi.org/v2/everything?q=apple&from=2024-11-18&to=2024-11-18&sortBy=popularity&apiKey=862fe9c21aff4c219d9f418f100d8b8d");
+                const response = await fetch("https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=862fe9c21aff4c219d9f418f100d8b8d");
                 const data = await response.json();
                 const article = data.articles;
-                const newData = article.splice(0, 3)
+                const newData = article.splice(3, 1)
                 const otherData = article.splice(1, 1)
-                const anotherData = article.splice(2, 1)
+                // const anotherData = article.splice(2, 1)
                 setData(article);
                 // console.log();
             } catch (error) {
